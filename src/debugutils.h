02ec1cfe7781f6fe23e510c4e8f5e97d2d7872b6
Sergio Contro <sergiocntr@gmail.com>
2018-08-23 17:02:58 +0200

    ok web mqtt

--- src/debugutils.h
+++ src/debugutils.h
@@ -1,8 +1,8 @@
 #ifndef DEBUGUTILS_H
 #define DEBUGUTILS_H
 #include <Arduino.h>
-//#define DEBUGMIO
-  #if defined DEBUGMIO
+//#define DEBUGU
+  #if defined DEBUGU
   #define DEBUG_PRINT(str)    \
      mySerial.print(millis());     \
      mySerial.print(": ");    \
@@ -10,31 +10,8 @@
      mySerial.print(__LINE__);     \
      mySerial.print(' ');      \
      mySerial.println(str);
-
-
-  #else
-  #define DEBUG_PRINT(str)
-    //mySerial.println(str);
-  #endif
-  #if defined DEBUGMIO
-  #define DEBUG_LCD(_buffer)    \
-     lcd.clear();               \
-     lcd.setCursor(0,0);        \
-     lcd.print(_buffer);        \
-     delay(2000);
-  #else
-  #define DEBUG_LCD(_buffer)
+#else
+  #define DEBUG_PRINT(str) \
+//    mySerial.println(str);
   #endif
 #endif
-/*
-#define DEBUG_PRINT(str)    \
-   mySerial.print(millis());     \
-   mySerial.print(": ");    \
-   mySerial.print(__PRETTY_FUNCTION__); \
-   mySerial.print(' ');      \
-   mySerial.print(__FILE__);     \
-   mySerial.print(':');      \
-   mySerial.print(__LINE__);     \
-   mySerial.print(' ');      \
-   mySerial.println(str);
-   */
